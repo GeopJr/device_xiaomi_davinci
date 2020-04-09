Davinci-Android-Development/android_device_xiaomi_davinci

davinci:  switched to HBM implementation
Co-Authored-By: Demon000 <demonsingur@gmail.com>

device.mk
@@ -70,6 +70,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_davinci
 
+PRODUCT_COPY_FILES += \
+    vendor/havoc/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \

GeopJr/device_xiaomi_davinci

device.mk
@@ -66,6 +66,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_davinci
 
+PRODUCT_COPY_FILES += \
+    vendor/aosp/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \