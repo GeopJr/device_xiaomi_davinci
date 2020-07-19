74f6efd1328702bb526f5ab62ef4d96196e7dc9b
GeopJr <geopjr@geopjr.xyz>
2020-07-19 23:34:43 +0300

    Bootleg-ify

--- bootleg_davinci.mk
+++ bootleg_davinci.mk
@@ -21,16 +21,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from davinci device
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Bootleggers stuff.
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := lineage_davinci
+PRODUCT_NAME := bootleg_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9T
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="davinci"
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Bootleg
+TARGET_PICK_BOOTANIMATION := 6
+
+DEVICE_MAINTAINERS := "GeopJr"
+
+BOOTLEGGERS_BUILD_TYPE := Shishufied
+
+# Even if we don't have notch, this centers wallpaper
+BOOTLEGGERS_NOTCHED := true
